40f03728f41237550cd00e9677a23082809b0382
Maria Karpikova <makarpikova@gmail.com>
2017-09-05 11:36:58 +0300

    deleting garbish

--- src/main/java/maven_jetty_test/App.java
+++ src/main/java/maven_jetty_test/App.java
@@ -1,12 +1,5 @@
 package maven_jetty_test;
 
-import org.eclipse.jetty.server.Server;
-import org.eclipse.jetty.servlet.ServletContextHandler;
-import org.eclipse.jetty.servlet.ServletHolder;
-import org.glassfish.jersey.server.ResourceConfig;
-import org.glassfish.jersey.servlet.ServletContainer;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
-
 public class App
 {
     public static void main( String[] args )
